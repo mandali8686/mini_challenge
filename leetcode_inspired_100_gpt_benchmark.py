15d48d07efabb108d7b60040943c50f141177ad2
manda <manda.li8686@gmail.com>
2023-09-11 08:56:27 -0500

    update with leetcode

---
+++ leetcode_inspired_100_gpt_benchmark.py
@@ -0,0 +1,219 @@
+
+import json
+
+# Metadata for problems
+problems_metadata = [
+    {
+        "id": 1,
+        "description": "Generalize the problem 'Two Sum' to work with various conditions.",
+        "tags": [
+            "Generalization",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Generalization"
+    },
+    {
+        "id": 2,
+        "description": "Generalize the problem 'Add Two Numbers' to work with various conditions.",
+        "tags": [
+            "Generalization",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Generalization"
+    },
+    {
+        "id": 3,
+        "description": "Generalize the problem 'Longest Substring Without Repeating Characters' to work with various conditions.",
+        "tags": [
+            "Generalization",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Generalization"
+    },
+    {
+        "id": 4,
+        "description": "Generalize the problem 'Median of Two Sorted Arrays' to work with various conditions.",
+        "tags": [
+            "Generalization",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Generalization"
+    },
+    {
+        "id": 5,
+        "description": "Generalize the problem 'Longest Palindromic Substring' to work with various conditions.",
+        "tags": [
+            "Generalization",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Generalization"
+    },
+    {
+        "id": 31,
+        "description": "Ensure fairness in the problem 'Next Permutation' by handling all edge cases.",
+        "tags": [
+            "Fairness",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Fairness"
+    },
+    {
+        "id": 32,
+        "description": "Ensure fairness in the problem 'Longest Valid Parentheses' by handling all edge cases.",
+        "tags": [
+            "Fairness",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Fairness"
+    },
+    {
+        "id": 33,
+        "description": "Ensure fairness in the problem 'Search in Rotated Sorted Array' by handling all edge cases.",
+        "tags": [
+            "Fairness",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Fairness"
+    },
+    {
+        "id": 34,
+        "description": "Ensure fairness in the problem 'Find First and Last Position of Element in Sorted Array' by handling all edge cases.",
+        "tags": [
+            "Fairness",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Fairness"
+    },
+    {
+        "id": 35,
+        "description": "Ensure fairness in the problem 'Search Insert Position' by handling all edge cases.",
+        "tags": [
+            "Fairness",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Fairness"
+    },
+    {
+        "id": 61,
+        "description": "Optimize the problem 'Rotate List' for better performance.",
+        "tags": [
+            "Optimization",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Optimization"
+    },
+    {
+        "id": 62,
+        "description": "Optimize the problem 'Unique Paths' for better performance.",
+        "tags": [
+            "Optimization",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Optimization"
+    },
+    {
+        "id": 63,
+        "description": "Optimize the problem 'Unique Paths II' for better performance.",
+        "tags": [
+            "Optimization",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Optimization"
+    },
+    {
+        "id": 64,
+        "description": "Optimize the problem 'Minimum Path Sum' for better performance.",
+        "tags": [
+            "Optimization",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Optimization"
+    },
+    {
+        "id": 65,
+        "description": "Optimize the problem 'Valid Number' for better performance.",
+        "tags": [
+            "Optimization",
+            "Automated Assessment"
+        ],
+        "expected_aspect": "Optimization"
+    }
+]
+
+# ==========================
+# Generalization Problems
+# ==========================
+
+
+def two_sum(args):
+    # Generalize the problem 'Two Sum' to work with various conditions.
+    pass
+
+def add_two_numbers(args):
+    # Generalize the problem 'Add Two Numbers' to work with various conditions.
+    pass
+
+def longest_substring_without_repeating_characters(args):
+    # Generalize the problem 'Longest Substring Without Repeating Characters' to work with various conditions.
+    pass
+
+def median_of_two_sorted_arrays(args):
+    # Generalize the problem 'Median of Two Sorted Arrays' to work with various conditions.
+    pass
+
+def longest_palindromic_substring(args):
+    # Generalize the problem 'Longest Palindromic Substring' to work with various conditions.
+    pass
+
+
+# ==========================
+# Fairness Problems
+# ==========================
+
+
+def next_permutation(args):
+    # Ensure fairness in the problem 'Next Permutation' by handling all edge cases.
+    pass
+
+def longest_valid_parentheses(args):
+    # Ensure fairness in the problem 'Longest Valid Parentheses' by handling all edge cases.
+    pass
+
+def search_in_rotated_sorted_array(args):
+    # Ensure fairness in the problem 'Search in Rotated Sorted Array' by handling all edge cases.
+    pass
+
+def find_first_and_last_position_of_element_in_sorted_array(args):
+    # Ensure fairness in the problem 'Find First and Last Position of Element in Sorted Array' by handling all edge cases.
+    pass
+
+def search_insert_position(args):
+    # Ensure fairness in the problem 'Search Insert Position' by handling all edge cases.
+    pass
+
+
+# ==========================
+# Optimization Problems
+# ==========================
+
+
+def rotate_list(args):
+    # Optimize the problem 'Rotate List' for better performance.
+    pass
+
+def unique_paths(args):
+    # Optimize the problem 'Unique Paths' for better performance.
+    pass
+
+def unique_paths_ii(args):
+    # Optimize the problem 'Unique Paths II' for better performance.
+    pass
+
+def minimum_path_sum(args):
+    # Optimize the problem 'Minimum Path Sum' for better performance.
+    pass
+
+def valid_number(args):
+    # Optimize the problem 'Valid Number' for better performance.
+    pass
+
